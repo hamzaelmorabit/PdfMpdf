98df1e889bf4fdfcc39b11cfc1e134e7488eae12
Hamza EL MORABIT <hamzamorabit123@gmail.com>
2021-05-26 14:47:55 +0100

    update table

--- test.php
+++ test.php
@@ -5,7 +5,122 @@
 // load dependencies via Composer
 require __DIR__ . '/vendor/autoload.php';
 
+// $jsonobj = '{"Peter":35,"Ben":37,"Joe":43}';
 
+// $obj = json_decode($jsonobj);
+
+// echo $obj->Peter;
+// echo $obj->Ben;
+// echo $obj->Joe;
+
+$string = file_get_contents("./data.json");
+$json = json_decode($string, true);
+$aaa = array(0 => 100);
+ $array = array(0 => 100, "color" => $aaa);
+print_r(($array["color"][0]));
+  // Convert JSON string to Object
+  $someObject  = json_decode(json_encode($json),true);
+
+  echo "<pre>";
+  print_r($someObject[0]["type"]);     
+  // echo "<pre>";
+  $item = array($someObject[1]["values"][1]) ;// Dump all data of the Object
+  // echo "<pre>";
+  // print_r(gettype($item));      // Dump all data of the Object
+   print_r(($item[0]));  
+
+   function tableSection($someObject,$column1){
+     $i = $column1;
+        $title = $someObject[0]["type"];
+     
+       $seTableStr = '<table width="100%"       ><tbody>';
+
+    foreach ($someObject[0]["values"] as $key=>$val){
+        $seTableStr .= '<tr>';
+         if(isset($val["type"]) && $i % 2 != 0 ) {
+           $seTableStr .= '<td style="font-weight:bold;padding-top:2%">'.$val["key"].'<td>';
+
+           if($val["type"] != 'image')
+               $seTableStr .= '<td style="padding-left:43%;font-weight:bold;">' .$val["value"] . '</td>';        
+           else  
+              $seTableStr .= '<td style="padding-left:43%"><img src="'.$val['value'].'" width="32%" /></td>';
+
+        }
+        $i++;
+     $seTableStr .= '</tr>';
+    
+  }
+
+    $seTableStr .= '</tbody></table>';
+    
+return [$seTableStr,$title];
+}
+    
+ function section($headerType,$column1,$column2){
+          $titleStyle="title";
+          if($headerType!=''){
+           $section =  '<div class='.$titleStyle.'>'.$headerType.'</div>';
+          }
+          $section .=  '
+            <table width="100%"
+            style="border-bottom: 0px solid #000000; 
+            vertical-align: top; 
+            font-family: serif; font-size: 11px;">
+            <tr>
+            <td width="33%;">'.$column1.'</td>
+
+            <td width="10% ;color:red"></td>  
+            <td width="33%;">'.$column2.'</td>
+            </tr></table>';
+
+          return $section;
+}
+$section = section(tableSection($someObject,1)[1],tableSection($someObject,1)[0],tableSection($someObject,0)[0]);
+
+ $x = 0;
+    $i =0;
+    $y = 0;
+  echo $section ;
+
+//     while (isset($patient_profile[$y][$x])) {
+//   echo "ff";
+      
+//         while (isset($patient_profile[$y][$x]) ) {
+//   echo "<pre>";
+
+// echo($patient_profile[$y][$x]);  
+//   echo "</pre>";
+          
+//                   $x++;
+//               }
+              
+//                 $x = 0;
+//                 $i++;
+//                 $y++;
+
+    // }
+// 
+        // Dump all data of the Object
+  // print_r(($item[1]));      // Dump all data of the Object
+  echo "<pre>";
+  print_r(sizeof ($someObject)); //      // Dump all data of the Object
+  echo "<pre>";
+//   echo $someObject[0]->first_name;
+
+// $array = json_decode($json, true);
+// print_r($array);
+// echo gettype($json[0]["values"]);
+// echo '<br />';
+// echo $json[0]["left_text"];
+// echo '<br />';
+// echo $json[0]["columns"];
+// echo '<br />';
+// echo $json[0]["right_text"];
+// echo '<br />';
+// echo $json[0]["values"][0]["type"];
+// echo '<br />';
+// echo '<br />';
+// echo '<br /> New line';
 
 // $dom = new Document();
 // $dom->loadFromUrl("http://localhost/testPhp/test.php");
@@ -16,7 +131,8 @@ require __DIR__ . '/vendor/autoload.php';
 // }
 /* foreach($nodes as $node) {
      echo "$node";
-    $classes = $node->getAttribute('name');
+    $classes = $node->    array("Braden score",' Consult clinical staff '),
+Attribute('name');
     echo "$classes";
 }
  */
@@ -133,7 +249,7 @@ chart.render();
   // array_map('htmlentities', $row);
    ?>
     <tr>
-      <td><?php echo implode('</td><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;', $row); ?></td>
+      <td style="font-weight: bold"><?php echo implode('</td><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;', $row); ?></td>
     </tr>
 <?php endforeach; ?>
   </tbody>
